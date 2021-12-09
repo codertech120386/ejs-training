f820d8cfb6bb2469deafa438c7e5ebf71aec9033
Dhaval Chheda <dhavalchheda@Dhavals-MacBook-Pro.local>
2021-12-09 14:36:26 +0530

    initial commit

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 
 module.exports = () => {
   return mongoose.connect(
-    "mongodb+srv://dhaval@secret_123@cluster0.3tgdz.mongodb.net/test"
+    "mongodb+srv://dhaval:secret_123@cluster0.3tgdz.mongodb.net/test"
   );
 };
